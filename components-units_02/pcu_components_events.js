2c17e42d74698811a0133e9d6ae75666a8531a34
Maayanp <61969711+Maayanp@users.noreply.github.com>
2021-11-08 11:47:08 +0200

    change to pcu

--- components-units_02/pcu_components_events.js
+++ components-units_02/pcu_components_events.js
@@ -8,40 +8,40 @@ function wsrComponentsEvents() {
   /* --------------------------- Search Field --------------------------- */
   document.querySelectorAll('.topbar-search-input').forEach(function(elm) {
     elm.addEventListener('focus', function(e) {
-      e.target.parentElement.classList.add('cpu-field-has-focus')
+      e.target.parentElement.classList.add('pcu-field-has-focus')
     });
 
     elm.addEventListener('focusout', function(e) {
-      e.target.parentElement.classList.remove('cpu-field-has-focus')
+      e.target.parentElement.classList.remove('pcu-field-has-focus')
     });
   });
 
 
   /* --------------------------- Dropdown --------------------------- */
   // dropdown focus
-  document.querySelectorAll('.cpu-dropdown .cpu-text-input').forEach(function(elm) {
+  document.querySelectorAll('.pcu-dropdown .pcu-text-input').forEach(function(elm) {
     elm.addEventListener('focus', function(e) {
-      e.target.parentElement.classList.add('cpu-field-has-focus')
+      e.target.parentElement.classList.add('pcu-field-has-focus')
     });
   });
 
   // select dropdown item
-  document.querySelectorAll('.cpu-dropdown-item').forEach(function(dropdownItem) {
+  document.querySelectorAll('.pcu-dropdown-item').forEach(function(dropdownItem) {
     dropdownItem.addEventListener('click', function(e) {
       const input = e.target.parentElement.previousElementSibling;
       input.value = e.target.getAttribute('value');
-      e.target.parentElement.querySelectorAll('.cpu-dropdown-item').forEach(function(sibilingItem) {
+      e.target.parentElement.querySelectorAll('.pcu-dropdown-item').forEach(function(sibilingItem) {
         sibilingItem.classList.remove('selected');
       })
       e.target.classList.add('selected');
-      e.target.parentElement.parentElement.classList.remove('cpu-field-has-focus');
+      e.target.parentElement.parentElement.classList.remove('pcu-field-has-focus');
     });
 
     // close dropdown
     document.addEventListener('click', function(e) {
-      if (!e.target.classList.contains('cpu-dropdown-input')) {
-        document.querySelectorAll('.cpu-dropdown').forEach(function(dropdowns) {
-          dropdowns.classList.remove('cpu-field-has-focus');
+      if (!e.target.classList.contains('pcu-dropdown-input')) {
+        document.querySelectorAll('.pcu-dropdown').forEach(function(dropdowns) {
+          dropdowns.classList.remove('pcu-field-has-focus');
         })
       }
     });
@@ -50,22 +50,22 @@ function wsrComponentsEvents() {
 
   /* --------------------------- Badge --------------------------- */
   // dropdown focus
-  document.querySelectorAll('.cpu-dropdown .cpu-text-input').forEach(function(elm) {
+  document.querySelectorAll('.pcu-dropdown .pcu-text-input').forEach(function(elm) {
     elm.addEventListener('focus', function(e) {
-      e.target.parentElement.classList.add('cpu-field-has-focus')
+      e.target.parentElement.classList.add('pcu-field-has-focus')
     });
   });
 
   // select dropdown item & and cahnge the background color
-  document.querySelectorAll('.cpu-dropdown[skin="badge"] .cpu-dropdown-content .cpu-dropdown-item').forEach(function(dropdownItem) {
+  document.querySelectorAll('.pcu-dropdown[skin="badge"] .pcu-dropdown-content .pcu-dropdown-item').forEach(function(dropdownItem) {
     dropdownItem.addEventListener('click', function(e) {
       const input = e.target.parentElement.previousElementSibling;
       input.value = e.target.getAttribute('value');
-      e.target.parentElement.querySelectorAll('.cpu-dropdown-item').forEach(function(sibilingItem) {
+      e.target.parentElement.querySelectorAll('.pcu-dropdown-item').forEach(function(sibilingItem) {
         sibilingItem.classList.remove('selected');
       })
       e.target.classList.add('selected');
-      e.target.parentElement.parentElement.classList.remove('cpu-field-has-focus');
+      e.target.parentElement.parentElement.classList.remove('pcu-field-has-focus');
       let color = e.target.children[0].getAttribute('color');
       input.setAttribute('color', color)
     });
@@ -74,7 +74,7 @@ function wsrComponentsEvents() {
 
   /* --------------------------- Float Notification --------------------------- */
   // remove this notification (after clicking on the x icon)
-  document.querySelectorAll('.cpu-float-notification .cpu-float-notification-close').forEach(function(elm) {
+  document.querySelectorAll('.pcu-float-notification .pcu-float-notification-close').forEach(function(elm) {
     elm.addEventListener('click', function(e) {
       e.target.parentElement.classList.add('remove-notification')
     });
@@ -82,24 +82,24 @@ function wsrComponentsEvents() {
 
 
   /* --------------------------- Popover Menu --------------------------- */
-  document.querySelectorAll('.cpu-popover-menu').forEach(function(popoverMenu) {
+  document.querySelectorAll('.pcu-popover-menu').forEach(function(popoverMenu) {
     popoverMenu.addEventListener('click', function(e) {
-      this.classList.toggle('cpu-open-popover');
+      this.classList.toggle('pcu-open-popover');
     });
   });
 
 
   /* --------------------------- Date Picker --------------------------- */
-  document.querySelectorAll('.cpu-calendar-day').forEach(function(dropdownItem) {
+  document.querySelectorAll('.pcu-calendar-day').forEach(function(dropdownItem) {
     dropdownItem.addEventListener('click', function(e) {
-      document.querySelectorAll('.cpu-calendar-day').forEach(function(day) {
+      document.querySelectorAll('.pcu-calendar-day').forEach(function(day) {
         day.classList.remove('selected');
       });
       const dayBtn = e.target.parentElement;
       if (!dayBtn.classList.contains('gray-day')) {
         dayBtn.classList.add('selected');
         let theDay = dayBtn.getAttribute('day');
-        dayBtn.closest('.cpu-date-picker').children[1].value = '09/' + theDay + '/2021';
+        dayBtn.closest('.pcu-date-picker').children[1].value = '09/' + theDay + '/2021';
       }
     });
   });
@@ -107,22 +107,22 @@ function wsrComponentsEvents() {
 
   /* --------------------------- Table --------------------------- */
   // open more menu
-  // document.querySelectorAll('.cpu-popover-menu').forEach(function(popoverMenu) {
+  // document.querySelectorAll('.pcu-popover-menu').forEach(function(popoverMenu) {
   //   popoverMenu.addEventListener('click', function (e) {
-  //     this.classList.toggle('cpu-open-popover');
+  //     this.classList.toggle('pcu-open-popover');
   //   });
   // });
 
     // select all checkbox
-    document.querySelectorAll('.cpu-table-select-all').forEach(function(popoverMenu) {
+    document.querySelectorAll('.pcu-table-select-all').forEach(function(popoverMenu) {
       popoverMenu.addEventListener('change', function(e) {
-        const table = e.target.parentNode.closest('.cpu-table');
+        const table = e.target.parentNode.closest('.pcu-table');
         if(e.target.checked) {
-          table.querySelectorAll('.cpu-checkbox-input').forEach(function(checkbox) {
+          table.querySelectorAll('.pcu-checkbox-input').forEach(function(checkbox) {
             checkbox.checked = true;
           });
         } else {
-          table.querySelectorAll('.cpu-checkbox-input').forEach(function(checkbox) {
+          table.querySelectorAll('.pcu-checkbox-input').forEach(function(checkbox) {
             checkbox.checked = false;
           });
         }
